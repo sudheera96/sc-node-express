553fa55180a93046ce4f176e3ec05a608f1c5586
Chitipolu Sri Sudheera <csrisudheera96@gmail.com>
2021-01-14 17:10:12 -0600

    updated app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello world from Sri Sudheera`s app!')
+})
+app.get('/about', (req, res) => {
+    res.send(' You have requested the about page! ')
+  })
+app.get('/contact', (req, res) => {
+    res.send(' You have requested the contact page! ')
+  })
+app.get('/help', (req, res) => {
+    res.send(' You have requested the help page! ')
+  })
+app.get('/help/:topic', (req, res) => {
+    res.send(` You have requested the help ${req.params.topic} page! `)
+  })
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
